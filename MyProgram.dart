fc8c2ccb29e9c7c46f78d176091a7a8a46ac841b
sako-ranj <95678848+sako-ranj@users.noreply.github.com>
2021-12-18 23:25:06 +0300

    Create MyProgram.dart

---
+++ MyProgram.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors 
+
+import 'package:flutter/material.dart'; 
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('MyProgram'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'A Centered Text',
+          style: TextStyle(
+              color: Colors.red,
+              fontSize: 25.0,
+              fontWeight: FontWeight.w300),
+        ),
+      ),
+    ),
+  ));
+}
+
+
+
+
+
+
+
+
